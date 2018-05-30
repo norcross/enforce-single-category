8d4e97ad92ae7c0cd8bd65e5c425cd129e1cec66
Norcross <andrew.norcross@gmail.com>
2018-05-30 17:15:10 -0400

    Freshly-scaffolded plugin via WP-CLI; initial commit.

---
+++ enforce-single-category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Enforce Single Category
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     enforce-single-category
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Enforce_Single_Category
+ */
+
+// Your code starts here.
